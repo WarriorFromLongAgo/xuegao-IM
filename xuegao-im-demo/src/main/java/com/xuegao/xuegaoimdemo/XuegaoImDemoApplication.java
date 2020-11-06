19fca5f675bb5ca7b12b2d7c80aa2cb855e31075
80004960 <80004960@sfmail.sf-express.com>
2020-11-06 17:56:26 +0800

    im demo WebSocket

---
+++ xuegao-im-demo/src/main/java/com/xuegao/xuegaoimdemo/XuegaoImDemoApplication.java
@@ -0,0 +1,18 @@
+package com.xuegao.xuegaoimdemo;
+
+import com.xuegao.xuegaoimdemo.Server.NettyServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class XuegaoImDemoApplication {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext configurableApplicationContext =
+                SpringApplication.run(XuegaoImDemoApplication.class, args);
+        NettyServer nettyServer = configurableApplicationContext.getBean(NettyServer.class);
+        nettyServer.start();
+    }
+
+}
---
+++ xuegao-im-demo/src/test/java/com/xuegao/xuegaoimdemo/XuegaoImDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.xuegaoimdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuegaoImDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
